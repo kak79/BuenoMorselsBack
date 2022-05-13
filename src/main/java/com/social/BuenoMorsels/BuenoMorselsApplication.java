659790afd3c75ee368365079580e8e33b465f389
Kimberly Kohel-Hayes <kohelka@gmail.com>
2022-05-13 09:41:39 -0700

    spring project initialized

---
+++ src/main/java/com/social/BuenoMorsels/BuenoMorselsApplication.java
@@ -0,0 +1,13 @@
+package com.social.BuenoMorsels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuenoMorselsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuenoMorselsApplication.class, args);
+	}
+
+}
