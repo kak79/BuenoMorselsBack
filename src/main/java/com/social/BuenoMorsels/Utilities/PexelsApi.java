972b5436b985064feb8f1ab25d7854a5ce64f98c
Kimberly Kohel-Hayes <kohelka@gmail.com>
2022-05-16 08:41:22 -0700

    updated files

--- src/main/java/com/social/BuenoMorsels/Utilities/PexelsApi.java
+++ src/main/java/com/social/BuenoMorsels/Utilities/PexelsApi.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -20,7 +23,7 @@ public class PexelsApi {
 
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-			Dotenv=dotenv = Dotenv.load();
+			Dotenv dotenv = Dotenv.load();
 			conn.setRequestMethod("GET");// hard coded my api key for now
 			conn.setRequestProperty("Authorization", dotenv.get("MY_ENV_VAR1"));
 			conn.connect();
