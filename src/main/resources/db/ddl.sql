f4c6561704cf068b20c1518f47d1ba60cd7d7efa
Kimberly Kohel-Hayes <kohelka@gmail.com>
2022-05-27 15:48:16 -0700

    ddl updates

--- src/main/resources/db/ddl.sql
+++ src/main/resources/db/ddl.sql
@@ -16,7 +16,6 @@ CREATE TABLE usr (
     meal_id INTEGER NOT NULL REFERENCES meal
 );
 
-
 CREATE TABLE post (
     post_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
     title VARCHAR(50),    
@@ -29,47 +28,48 @@ CREATE TABLE post (
     sent_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
 
-
-CREATE TABLE public.review_likes (
-    liked boolean NOT NULL,
-    user_id integer NOT NULL,
-    review_id integer NOT NULL
+CREATE TABLE restaurant (
+    restaurant_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    restaurant_name VARCHAR(50) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    restaurant_rating VARCHAR(10) NOT NULL,
+    cusine VARCHAR(50) NOT NULL
 );
 
-
-CREATE TABLE public.user_comment (
-    id integer NOT NULL,
-    review_id integer,
-    user_id integer,
-    comment_text character varying(50) NOT NULL,
-    sent_at timestamp without time zone NOT NULL
+CREATE TABLE post_likes (
+    liked BOOL NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES usr,
+    post_id INTEGER NOT NULL REFERENCES post
 );
 
+CREATE TABLE rol (  --role?
 
-CREATE TABLE public.user_rank (
-    id integer NOT NULL,
-    rank_title character varying(50) NOT NULL
 );
 
+CREATE TABLE meal (
 
-
-
-CREATE TABLE public.movie (
-    id integer NOT NULL,
-    movie_name character varying(50) NOT NULL,
-    movie_year character varying(50) NOT NULL,
-    description character varying(200) NOT NULL,
-    movie_rating character varying(10) NOT NULL,
-    genre character varying(50) NOT NULL,
-    image character varying
 );
 
+--CREATE TABLE public.user_comment (
+--    id integer NOT NULL,
+--    review_id integer,
+--    user_id integer,
+--    comment_text character varying(50) NOT NULL,
+--    sent_at timestamp without time zone NOT NULL
+--);
 
-CREATE TABLE public.movie_rating (
-    rating integer NOT NULL,
-    user_id integer NOT NULL,
-    movie_id integer NOT NULL
-);
+
+--CREATE TABLE public.user_rank (
+--    id integer NOT NULL,
+--    rank_title character varying(50) NOT NULL
+--);
+
+
+--CREATE TABLE public.movie_rating (
+--    rating integer NOT NULL,
+--    user_id integer NOT NULL,
+--    movie_id integer NOT NULL
+--);
 
 
 
