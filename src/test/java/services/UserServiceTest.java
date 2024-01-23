1de56b34ad608ad9e16f6abd27267430404558f4
Kimberly Kohel-Hayes <kohelka@gmail.com>
2024-01-23 13:24:08 -0600

    started testing

---
+++ src/test/java/services/UserServiceTest.java
@@ -0,0 +1,33 @@
+package services;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.social.BuenoMorsels.BuenoMorselsApplication;
+import com.social.BuenoMorsels.Beans.User;
+import com.social.BuenoMorsels.Repository.UserRepo;
+import com.social.BuenoMorsels.Services.UserService;
+
+@SpringBootTest(classes=BuenoMorselsApplication.class)
+public class UserServiceTest {
+	
+	@MockBean
+	private UserRepo userRepo;
+	@Autowired
+	private UserService userServ;
+	
+
+	private static Set<User> mockUsers;
+	
+	@BeforeAll
+	public static void mockUsersSetup() {
+		
+	}
+	
+	
+	
+}
